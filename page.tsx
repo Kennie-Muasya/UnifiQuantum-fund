da7c15e70848122ba024f29d2745467dc029be75
Kennie-Muasya <kennmuasya49@gmail.com>
2026-06-04 12:23:48 +0300

    Create HomePage component with multiple sections

---
+++ page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/home/Hero";
+import ResearchLab from "@/components/home/ResearchLab";
+import DashboardPreview from "@/components/home/DashboardPreview";
+import Features from "@/components/home/Features";
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <ResearchLab />
+      <DashboardPreview />
+      <Features />
+    </>
+  );
+}
